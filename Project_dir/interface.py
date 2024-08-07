249311681430dddf754ca648aec2155a847156ea
Cristian Curran <cmc410@txstate.edu>
2024-08-06 18:39:33 -0700

    Updated a bunch of little stuff but nothing too big just yet...

--- Project_dir/interface.py
+++ Project_dir/interface.py
@@ -7,6 +7,10 @@ import pandas as pd
 from pandastable import Table
 import warnings
 
+#non-tkinter vars (global)
+path = ""
+toolbar_bool = False
+
 def run_gui():
     #ignore useless warnings of impending doom (i.e., future deprecated pandas functions)
     warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -21,14 +25,13 @@ def run_gui():
         path = askopenfilename(initialdir="Project_dir", filetypes=[("Comma Separated Values", "*.csv")])
         return path
 
-    def load_data():
+    def load_data(t_bool):
         path = get_path()
         df = pd.read_csv(path) #path was redefined to match the path directory returned in open_csv()
-        table = pt = Table(viewer_frame, dataframe=df, showtoolbar=False, showstatusbar= True)
+        pt = Table(viewer_frame, dataframe=df, showtoolbar=t_bool, showstatusbar= True)
         pt.show()
         pt.redraw()
-
-
+        return pt
 
     #objects
     window = tk.Tk()
@@ -36,9 +39,14 @@ def run_gui():
     #menu
     menubar = tk.Menu(window)
     filemenu = tk.Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Import", command=load_data)
+    filemenu.add_command(label="Import", command=lambda:load_data(t_bool=toolbar_bool))
+    filemenu.add_separator()
     filemenu.add_command(label="Exit", command=window.quit)
+    viewmenu = tk.Menu(menubar, tearoff=0)
+    viewmenu.add_checkbutton(label="Show Toolbar", onvalue=1, offvalue=0, variable=toolbar_bool)
+    viewmenu.add_separator()
     menubar.add_cascade(label="File", menu=filemenu)
+    menubar.add_cascade(label="View", menu=viewmenu)
 
     #frames
     main_frame = tk.Frame(window)
@@ -46,11 +54,11 @@ def run_gui():
     query_frame = tk.Frame(main_frame, height=25, padx=5, pady=10, bg = "green")
     text_entry = tk.Entry(query_frame, text= "Insert query here...", width=500)
     search_button = tk.Button(query_frame, text="Search", width=10)
-
-    #non-tkinter vars
-    icon = tk.PhotoImage(file="Project_dir/res/sloth_png.png")
+    
+    #non-tkinter vars (local)
+    icon = tk.PhotoImage(file="Project_dir/res/images/sloth_sleuth.png")
     query_string = tk.StringVar(master=query_frame)
-    path = ""
+    pt = Table(viewer_frame)
 
     #modify vars
     text_entry.configure(textvariable=query_string)
