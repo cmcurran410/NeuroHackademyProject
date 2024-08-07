cef3c0e79f17bb7174549148ca4050cdf2387b94
Cristian Curran <cmc410@txstate.edu>
2024-08-07 16:55:50 -0700

    Added some tool icons and reorganized the panels. Still have to make it to where the panels actually show, which is not the case for some reason.

--- Project_dir/interfaceWX.py
+++ Project_dir/interfaceWX.py
@@ -27,8 +27,12 @@ class SlothSleuth(wx.Frame):
         
         # View menu
         viewMenu = wx.Menu()
-        self.toolbarView = viewMenu.AppendCheckItem(wx.ID_ANY, 'Toolbar')
-        self.toolbarView.Check(check = True)
+        dfViewItem = viewMenu.AppendCheckItem(wx.ID_ANY, 'DataFrame View')
+        chatViewItem = viewMenu.AppendCheckItem(wx.ID_ANY, 'AI Chat View')
+        toolbarView = viewMenu.AppendCheckItem(wx.ID_ANY, 'Toolbar')
+        dfViewItem.Check(check=True)
+        chatViewItem.Check(check=True)
+        toolbarView.Check(check = True)
         menubar.Append(viewMenu, '&View')
 
         self.SetMenuBar(menubar)
@@ -36,54 +40,92 @@ class SlothSleuth(wx.Frame):
         # Bind menu events
         self.Bind(wx.EVT_MENU, self.OnImport, importItem)
         self.Bind(wx.EVT_MENU, self.OnQuit, quitItem)
-        self.Bind(wx.EVT_MENU, self.OnToggleToolbar, self.toolbarView)
-        
+        self.Bind(wx.EVT_MENU, self.OnToggleDFView, dfViewItem)
+        self.Bind(wx.EVT_MENU, self.OnToggleChatView, chatViewItem)
+
         # Main panel
-        panel = wx.Panel(self)
-        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        self.panel = wx.Panel(self)
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
 
-        # Query frame
-        queryFrame = wx.Panel(panel)
-        querySizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.queryString = wx.TextCtrl(queryFrame, value="Insert query here...", size=(500, -1))
-        searchButton = wx.Button(queryFrame, label='Search', size=(100, -1))
-        querySizer.Add(self.queryString, proportion=1, flag=wx.ALL | wx.EXPAND, border=5)
-        querySizer.Add(searchButton, flag=wx.ALL, border=5)
-        queryFrame.SetSizer(querySizer)
-
-        # Viewer frame
-        viewerFrame = wx.Panel(panel)
-        viewerSizer = wx.BoxSizer(wx.VERTICAL)
-        self.grid = gridlib.Grid(viewerFrame)
-        self.grid.CreateGrid(0, 0)
-        viewerSizer.Add(self.grid, proportion=1, flag=wx.ALL | wx.EXPAND, border=5)
-        viewerFrame.SetSizer(viewerSizer)
-
-        # Add frames to main sizer
-        mainSizer.Add(viewerFrame, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
-        mainSizer.Add(queryFrame, flag=wx.ALL | wx.EXPAND, border=10)
-        panel.SetSizer(mainSizer)
+        #Initial background panel
+        self.backgroundPanel = wx.Panel(self.panel)
+        self.backgroundImage = wx.Image("Project_dir/res/images/sloth_background.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.backgroundBitmap = wx.StaticBitmap(self.backgroundPanel, -1, self.backgroundImage)
+        self.vbox.Add(self.backgroundPanel, 1, wx.EXPAND)
         
-        # Status bar
+        self.panel.SetSizer(self.vbox)
+
+        #Status bar
         self.CreateStatusBar()
-        
-        # Toolbar
+
+        #Create Toolbar
         self.toolbar = self.CreateToolBar()
-        self.toolbar.AddTool(wx.ID_OPEN, 'Sloth', wx.Bitmap("Project_dir/res/images/sloth_sleuth.png"))
+        self.dfViewTool = self.toolbar.AddTool(wx.ID_ANY, 'DataFrame View', wx.Bitmap("Project_dir/res/images/dficon.png"))
+        self.chatViewTool = self.toolbar.AddTool(wx.ID_ANY, 'AI Chat View', wx.Bitmap("Project_dir/res/images/chaticon.png"))
         self.toolbar.SetToolBitmapSize(size=(32, 32))
         self.toolbar.Realize()
 
+        #Bind toolbar functions
+        self.Bind(wx.EVT_TOOL, self.OnToggleChatView, self.chatViewTool)
+        self.Bind(wx.EVT_TOOL, self.OnToggleDFView, self.dfViewTool)
+
     def OnImport(self, event):
-        with wx.FileDialog(self, "Open CSV file", wildcard="CSV files (*.csv)|*.csv",
-                           style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+        with wx.FileDialog(self, "Open CSV file", wildcard="CSV files (*.csv)|*.csv", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 return  # the user changed their mind
 
             path = fileDialog.GetPath()
             self.LoadData(path)
+            self.DisplayViews()
 
     def LoadData(self, path):
-        df = pd.read_csv(path)
+        self.df = pd.read_csv(path)
+
+    def DisplayViews(self):
+        self.vbox.Clear(True)
+        
+        # Search bar at the top (shared across views)
+        self.queryString = wx.TextCtrl(self.panel, value="Insert query here...", size=(500, -1))
+        searchButton = wx.Button(self.panel, label='Search', size=(100, -1))
+        searchButton.Bind(wx.EVT_BUTTON, self.OnSearch)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox.Add(self.queryString, 1, wx.EXPAND | wx.ALL, 5)
+        hbox.Add(searchButton, 0, wx.ALL, 5)
+        self.vbox.Add(hbox, 0, wx.EXPAND)
+        
+        # Splitter window for different views
+        self.splitter = wx.SplitterWindow(self.panel)
+        
+        # DataFrame view
+        self.dfPanel = wx.Panel(self.splitter)
+        self.dfSizer = wx.BoxSizer(wx.VERTICAL)
+        self.grid = gridlib.Grid(self.dfPanel)
+        self.grid.CreateGrid(0, 0)
+        self.dfSizer.Add(self.grid, 1, wx.EXPAND)
+        self.dfPanel.SetSizer(self.dfSizer)
+        
+        # AI Chat view
+        self.chatPanel = wx.Panel(self.splitter)
+        self.chatSizer = wx.BoxSizer(wx.VERTICAL)
+        self.chatBox = wx.TextCtrl(self.chatPanel, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.chatSizer.Add(self.chatBox, 1, wx.EXPAND)
+        self.inputBox = wx.TextCtrl(self.chatPanel, style=wx.TE_PROCESS_ENTER)
+        self.inputBox.Bind(wx.EVT_TEXT_ENTER, self.OnChatEnter)
+        self.chatSizer.Add(self.inputBox, 0, wx.EXPAND)
+        self.chatPanel.SetSizer(self.chatSizer)
+        
+        self.splitter.SplitVertically(self.dfPanel, self.chatPanel)
+        self.splitter.SetSashGravity(0.5)
+        self.vbox.Add(self.splitter, 1, wx.EXPAND)
+        
+        # Load data into the DataFrame view
+        self.LoadGridData()
+
+        self.panel.SetSizer(self.vbox)
+        self.Layout()
+
+    def LoadGridData(self):
+        df = self.df
         self.grid.ClearGrid()
 
         if self.grid.GetNumberRows() > 0:
@@ -103,9 +145,32 @@ class SlothSleuth(wx.Frame):
     def OnQuit(self, event):
         self.Close()
 
+    def OnSearch(self, event):
+        query = self.queryString.GetValue()
+        wx.MessageBox(f'Searching for: {query}', 'Search', wx.OK | wx.ICON_INFORMATION)
+
+    def OnChatEnter(self, event):
+        message = self.inputBox.GetValue()
+        self.chatBox.AppendText(f'You: {message}\n')
+        self.inputBox.Clear()
+    
     def OnToggleToolbar(self, event):
         if self.toolbar.IsShown():
             self.toolbar.Hide()
         else:
             self.toolbar.Show()
+        self.Layout()
+    
+    def OnToggleDFView(self, event):
+        if self.dfPanel.IsShown():
+            self.dfPanel.Hide()
+        else:
+            self.dfPanel.Show()
+        self.Layout()
+    
+    def OnToggleChatView(self, event):
+        if self.chatPanel.IsShown():
+            self.chatPanel.Hide()
+        else:
+            self.chatPanel.Show()
         self.Layout()
